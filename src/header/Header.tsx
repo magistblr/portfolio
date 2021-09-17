1ca67f0fda42f9d55ddc427166c72c27ba8b21b9
magistblr <78726323+magistblr@users.noreply.github.com>
2021-09-17 12:49:13 +0300

    mobile

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -16,11 +16,6 @@ export const Header = () => {
 
   return (
     <div className={s.header} id={"home"}>
-      <div className={s.svg_bg}>
-                <svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
-                    <polygon points="-30,300 355.167,210.5 1432.5,290 1920,198.5 1920,300"></polygon>
-                </svg>
-            </div>
       <div className={`${sContainer.container} ${s.container}`}>
     			<div className={s.row}>
               <h1 className={s.text}>
@@ -38,12 +33,15 @@ export const Header = () => {
                 </p>
               </h1>
     			</div>
-
           <div className={s.arrow}>
                 <div onClick={()=> {scrollToAbout()}}><img src="http://www.innovationplans.com/idesign/kewll/img/down-chevron.svg" alt=""/></div>
           </div>
-
     		</div>
+      <div className={s.svg_bg}>
+                <svg x="0px" y="0px" viewBox="0 186.5 1920 113.5">
+                    <polygon points="-30,300 355.167,210.5 1432.5,290 1920,198.5 1920,300"></polygon>
+                </svg>
+      </div>
     </div>
   );
 }
