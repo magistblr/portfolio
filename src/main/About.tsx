b931f0fbc24d231c08d055baa159ad7b3909bb59
magistblr <78726323+magistblr@users.noreply.github.com>
2021-10-14 17:17:16 +0300

    cv

--- src/main/About.tsx
+++ src/main/About.tsx
@@ -13,7 +13,7 @@ export const About = () => {
             <h4>I'm Yefeshin Maksim</h4>
             <span>FRONT-END DEVELOPER</span>
             <p>Hey! I'll tell you a little about what I do. I mainly write in ReactJS, below you can see examples of my work. There are also small projects made in native JS. If you are interested in my code, you can see it in my github profile, the link to it is at the very bottom.</p>
-            <a className={s.btn} href="https://drive.google.com/file/d/14bNUen2BopsfsIICdGDZ5aRlvP1TUy2O/view?usp=sharing">CV</a>
+            <a className={s.btn} href="https://drive.google.com/file/d/1UnUM9Tjwb6spqmNsLviA2Xxr_VGBv7ml/view?usp=sharing">CV</a>
           </div>
       </div>
       <div className={s.settings}>
